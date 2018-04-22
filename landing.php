53f91ca82a511b99e9e873e36182320e9cc64421
Rodolfo Reynoso <Rodolfo.Reynoso@icloud.com>
2018-04-22 15:15:27 -0400

    Landing Page and captcha updates

---
+++ landing.php
@@ -0,0 +1,17 @@
+<?php ?>
+
+<div class="landing-page__container">
+	<div class="landing-page__top-section">
+		<div class="landing-page__text-section">
+			<img src="assets/imgs/free-movie-ticket-txt.png">
+			<p class="landing-page__subtext">WHEN YOU BUY THREE SPECIALLY-MARKED PACKS OF JUICY JUICE! PLUS, BE ENTERED FOR A CHANCE TO</p>
+			<img src="assets/imgs/incredible-family-adventure-txt.png">
+		</div>
+		<div class="landing-page__promo-img-container">
+			<img src="assets/imgs/juicyJuice-bundle-large.png">		
+		</div>
+	</div>
+	<div class="landing-page__enter-btn-container">
+			<a href="#"><img src="assets/imgs/get-started-btn.png"></a>
+	</div>
+</div>
