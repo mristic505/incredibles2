f95ea9fe5cefdfe3532898fc29d270a2bf62ad5c
Rodolfo Reynoso <Rodolfo.Reynoso@icloud.com>
2018-04-21 22:34:51 -0400

    Add normalize.css and user style sheet to header.php

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <?php ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title><?php echo $page_title; ?> | Incredibles 2</title>
-</head>
-<body>
-<head>
-	
-</head>
+	<head>
+		<title><?php echo $page_title; ?> | Incredibles 2</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Normalize CSS -->
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+		<!-- User Stylesheet -->
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+	</head>
+	<body>
 <main>
