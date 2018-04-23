f1a25bdd857b6643fde4eca72b43b4f70360d5d6
Rodolfo Reynoso <rreynoso@brandconnections.com>
2018-04-23 12:42:36 -0400

    Update fonts and popup for mobile

--- landing.php
+++ landing.php
@@ -4,7 +4,8 @@
 	<div class="landing-page__top-section">
 		<div class="landing-page__text-section">
 			<img src="assets/imgs/free-movie-ticket-txt.png">
-			<p class="landing-page__subtext">WHEN YOU BUY THREE SPECIALLY-MARKED PACKS OF JUICY JUICE! PLUS, BE ENTERED FOR A CHANCE TO</p>
+			<img class="landing-page_subtext" src="assets/imgs/landing-page-txt.png">
+			<!-- <p class="landing-page__subtext">WHEN YOU BUY THREE SPECIALLY-MARKED PACKS OF JUICY JUICE! PLUS, BE ENTERED FOR A CHANCE TO</p> -->
 			<img src="assets/imgs/incredible-family-adventure-txt.png">
 		</div>
 		<div class="landing-page__promo-img-container">
@@ -12,6 +13,6 @@
 		</div>
 	</div>
 	<div class="landing-page__enter-btn-container">
-			<a href="#"><img src="assets/imgs/get-started-btn.png"></a>
+			<a href="?page=enter"><img src="assets/imgs/get-started-btn.png"></a>
 	</div>
 </div>
