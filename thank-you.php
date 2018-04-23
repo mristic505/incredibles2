5aacfd290398d6b75cb89e81f773217da69f716a
Rodolfo Reynoso <Rodolfo.Reynoso@icloud.com>
2018-04-22 23:03:17 -0400

    Add thank you page

--- thank-you.php
+++ thank-you.php
@@ -1,3 +1,13 @@
 <?php ?>
 
-<h1>THANK YOU!</h1>
+<div class="thank-you__container green-bg-gradient">
+	<img src="assets/imgs/thank-you-for-entering-txt.png">
+	<h2 class="thank-you__title">Once your codes are verified, you will receive an email from <span class="white">juicyjuice@brandconnections.com</span> with directions to redeem your Fandango movie ticket. From there, select your theater and show time and enjoy the movie!</h2>
+	<p class="social-text">SHARE ON SOCIAL!</p>
+	<div class="social-media-container">
+		<a href="#"><img src="assets/imgs/facebook-logo.png"></a>
+		<a class="socia-media__horozontal-margin" href="#"><img src="assets/imgs/twitter-logo.png"></a>
+		<a href="#"><img src="assets/imgs/instagram-logo.png"></a>
+	</div>	
+		<img src="assets/imgs/juicyJuice-bundle-large.png">
+</div>
