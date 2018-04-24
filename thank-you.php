0cdd12171969e318ae79da7d63ee1fde5d511ebb
Rodolfo Reynoso <rreynoso@brandconnections.com>
2018-04-24 14:59:28 -0400

    Add twitter share link

--- thank-you.php
+++ thank-you.php
@@ -6,7 +6,7 @@
 	<img class="social-text" src="assets/imgs/share-on-social-txt.png">
 	<div class="social-media-container">
 		<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A//juicyjuice.staging.wpengine.com/incredibles2/"><img src="assets/imgs/facebook-logo.png"></a>
-		<a class="socia-media__horozontal-margin" href="#"><img src="assets/imgs/twitter-logo.png"></a>
+		<a class="socia-media__horozontal-margin" href="https://twitter.com/intent/tweet?url=&text=I%20entered%20the%20Incredible%20Family%20Adventure%20Offer%20and%20Sweepstakes%20and%20got%20a%20FREE%20movie%20ticket.%20*FANDANGO%20PROMO%20CODE%20expires%203%2F31%2F19.%20Up%20to%20%247.50%20value.%20See%20www.juicyjuice.com%2Fincredibles2%20for%20full%20details.%20While%20supplies%20last."><img src="assets/imgs/twitter-logo.png"></a>
 	</div>	
 		<img src="assets/imgs/juicyJuice-bundle-large.png">
 </div>
