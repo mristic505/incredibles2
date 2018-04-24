5870f372cd637412456ff237909f00d59b4f0d5b
Mateja Ristic <mristic@brandconnections.com>
2018-04-24 20:44:54 +0200

    add FB share link to thank you page

--- thank-you.php
+++ thank-you.php
@@ -5,7 +5,7 @@
 	<h2 class="thank-you__title">Once your codes are verified, you will receive an email from <span class="white">juicyjuice@brandconnections.com</span> with directions to redeem your Fandango movie ticket. From there, select your theater and show time and enjoy the movie!</h2>
 	<img class="social-text" src="assets/imgs/share-on-social-txt.png">
 	<div class="social-media-container">
-		<a href="#"><img src="assets/imgs/facebook-logo.png"></a>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//juicyjuice.staging.wpengine.com/incredibles2/"><img src="assets/imgs/facebook-logo.png"></a>
 		<a class="socia-media__horozontal-margin" href="#"><img src="assets/imgs/twitter-logo.png"></a>
 	</div>	
 		<img src="assets/imgs/juicyJuice-bundle-large.png">
