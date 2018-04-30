8f88038cd3df4beff6aa91ac597f18a7ed2852dd
Mateja Ristic <mristic@brandconnections.com>
2018-04-30 22:50:08 +0200

    add GA code

--- header.php
+++ header.php
@@ -17,6 +17,19 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src=https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js></script>
 <script type="text/javascript" src="js/scripts.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58591031-1', 'auto', {'allowLinker': true});
+  ga('require', 'linker');
+  ga('linker:autoLink', ['juicyjuicesplashers.com'] );
+  ga('send', 'pageview');
+
+</script>
+
 </head>
 <body>
 <?php echo $flood_light; ?>
